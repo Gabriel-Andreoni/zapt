98c5d079325263ad6d8a4fe581aa1a1241698991
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2026-04-17 10:40:40 -0300

    Alteração da cor do botão da hero section

--- app/sections/Hero/index.tsx
+++ app/sections/Hero/index.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
                     <PageTitle title="Olá, nós somos a Zapt." color="white" />
                     <h1 className="text-6xl font-bold text-white">Transformamos ideias em experiência digitais</h1>
                     <h2 className="py-4 text-lg text-white">Desenvolvemos sistemas web, aplicativos e soluções personalizadas com as tecnologias mais modernas do mercado para destacar sua empresa.</h2>
-                    <button className="w-3/12 p-3 text-lg bg-[#0095DA] rounded text-white cursor-pointer">Saiba mais</button>
+                    <button className="w-3/12 p-3 text-lg bg-[#0554f2] rounded text-white cursor-pointer">Saiba mais</button>
                 </div>
             </div>
         </section>
