92fbba57560707d48decac9a44853d034ad85d7d
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2026-04-17 10:38:55 -0300

    Alterações no menu e na hero section

--- app/sections/Menu/index.tsx
+++ app/sections/Menu/index.tsx
@@ -10,11 +10,11 @@ export default function Menu() {
         { name: "Contato", href: "#" }
     ]
     return (
-        <header className="w-full h-30 flex items-center bg-white">
+        <header className="w-full h-30 flex items-center bg-[#ffffff20] backdrop-blur-2xl">
             <nav className="w-6/12 h-full pl-48 flex items-center gap-24">
                 <Image src="/logo.png" alt="logo da zapt" width={64} height={64} />
                 <ul className="w-full p-6 flex justify-start gap-24">
-                    {items.map((i, index) => <li key={index} className="menu-item h-fit text-xl cursor-pointer text-black relative">{i.name}</li>)}
+                    {items.map((i, index) => <li key={index} className="menu-item h-fit text-xl cursor-pointer text-white relative">{i.name}</li>)}
                 </ul>
             </nav>
         </header>
